356fbdeb48d59bc72a9f6a5544cd70ee6a56aefd
Hans <bmm79@nau.edu>
2018-05-24 01:33:55 -0700

    stuff and things

---
+++ hs.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const { prompt } = require('inquirer');
+
+const { 
+	questions, 
+	dnsQuestions 
+} = require('./lib/questions.js');
+
+const { 
+  addDomain, 
+  dnsDomain, 
+  // getDomainList,
+  // updateDomains,
+  // deleteDomains  
+} = require('./lib/domain'); 
+
+program
+  .version('0.0.1')
+  .description('Hosting management system')
+
+// adds a domain to the db
+program
+  .command('addDomain')
+  .alias('d')
+  .description('Add Domain')
+  .action(() => {
+    prompt(questions).then((answers) =>
+      addDomain(answers));
+  });
+
+// check the dns records of a domain (dns propogation / history)
+program
+  .command('dnsDomain')
+  .alias('dns')
+  .description('Checking Domain dns')
+  .action(() => {
+    prompt(dnsQuestions).then((answers) =>
+      dnsDomain(answers));
+  });
+
+
+
+
+// Assert that a VALID command is provided 
+if (!process.argv.slice(2).length || !/[arudl]/.test(process.argv.slice(2))) {
+  program.outputHelp();
+  process.exit();
+}
+program.parse(process.argv)
+    
