3ad0d2f3d1eadb86b381cec5c5c6832792a00ead
Hans <bmm79@nau.edu>
2018-06-13 01:20:44 -0700

    rm humor? $lame : $lol;
    rewrite with one liners

--- php/WP_500.php
+++ php/WP_500.php
@@ -1,49 +1,38 @@
 <?php
-////
-//// This file will self-destruct after running. 
-//// Good luck Agents.
-////
 
+/**
+ * Settings
+ */
+
+$WP_500   	= true;				
+$WP_AC 	 	= ".htaccess";
+$WP_WC 	 	= "wp-content";
+$WP_mu 		= $WP_WC."mu-plugins";
+$WP_obj_c 	= $WP_WC."/object-cache.php";
 
 // current time
-echo "Begin the 500 error test for WP...". date('h:i:s') . "\n";
+echo "Begin the 500 error test...";
 
 
-//// check if .htaccess exists, then rename it
-	$GDaccess = '.htaccess';
+/**
+ * Disable the .htaccess file, plugin folder, mu_plugins && cache
+ */
+if ( $WP_500 === true ) {
+	echo (file_exists($WP_AC)) ? rename($WP_AC, $WP_AC.".bad"):  "access file !exist||renamed...";
+	echo (file_exists($WP_WC."/plugins")) ? rename($WP_WC."/plugins", $WP_WC."/plugins"."-bad"):"plugin folder !exist||renamed...";
+	echo (file_exists($WP_mu)) ? rename($WP_mu, $WP_mu."-bad"):"mu-plugin folder !exist||renamed...";
+	echo (file_exists($WP_obj_c)) ? rename($WP_obj_c, $WP_obj_c.".bad"):"cache !exist||renamed...";
+}
 
-	if (file_exists($GDaccess)) {
-	    echo "... The file $GDaccess exists and has been renamed."."<br>";
-	    rename("$GDaccess", "$GDaccess".".bad");
-	} else {
-	    echo "... The file $GDaccess does not exist";
-		if (file_exists(".htaccess.bad")) {
-		    echo "... test file exists, putting it back...";
-		    rename(".htaccess.bad", ".htaccess");
-		}
+/**
+ * This will undo everything and then remove the test file
+ */
+	if (!$WP_500 ) {
+		echo (file_exists($WP_AC.".bad")) ? rename($WP_AC.".bad",$WP_AC): "access.bad file !exist||renamed...";
+		echo (file_exists($WP_WC."/plugins-bad")) ? rename($WP_WC."/plugins-bad", $WP_WC."/plugins"):"plugin-bad !exist||renamed...";
+		echo (file_exists($WP_mu."-bad")) ? rename($WP_mu."-bad", $WP_mu):"mu-plugin folder !exist||renamed...";
+		echo (file_exists($WP_obj_c.".bad")) ? rename($WP_obj_c.".bad", $WP_obj_c):"cache !exist||renamed...";
+		unlink('WP_500.php');
 	}
-
-//// Disable the plugins
-	if (file_exists('wp-content/plugins')) {
-	    rename('wp-content/plugins', 'wp-content/plugins-bad');
-	    echo "\n The plugin folder has been renamed.";
-	} else {
-	    echo "... There is no wp-content folder...";
-		if (file_exists('wp-content/plugins-bad')) {
-		    echo "... test file exists, putting it back...";
-		    rename("wp-content/plugins-bad", "wp-content/plugins");
-
-		}
-	}
-
-//// If all tests were run
-	if (file_exists(".htaccess.bad")&&file_exists('wp-content/plugins-bad')) {
-		echo "... BOTH Test files detected, putting everything back together and SELF DESTRUCTING...";
-		unlink('testfile.php');
-	}
-
-
-
-
 
 ?>
